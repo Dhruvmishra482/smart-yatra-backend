823b84061c13954b9eb05a221763af831c0a2b8e
dhruvmishra482 <dhruvmishra3829@gmail.com>
2025-07-27 16:11:55 +0530

    Add Chandigarh-Delhi and Delhi-Mumbai transport data (bus, train, flight)

--- controllers/searchFare.js
+++ controllers/searchFare.js
@@ -1,6 +1,4 @@
-const fs = require("fs").promises;
-const path = require("path");
-const SearchQuery = require("../models/SearchQuerySchema");
+
 
 const fs = require("fs/promises");
 const path = require("path");
